92996ffd2919a3007dfffb1ced818d9a069a3ead
SisolusMaximus <jaimperator99@gmail.com>
2021-04-02 18:17:09 +0200

    "Set up navigation on menu items using withRouter"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 
+
+const HatsPage = () =>(
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact={true} path='/' component={HomePage}/>
+        <Route exact={true} path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
