953c35aec11eba074e6604c7c5c63fa7855aef8e
SisolusMaximus <jaimperator99@gmail.com>
2021-04-28 19:32:16 +0200

    added express server for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,10 +1,24 @@
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({price}) =>{
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51Idzo1IJD3612PvFd10bvcmkEHeEmmA5am8Tjjy8glwyCCm10OuNjnNQKxEz80QlIgfvr3GQrED3Vo4L1pKuN0UZ00v7pkppnd'
-    const onToken = token => {console.log(token);
-                              alert('Payment Successful');}
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res =>{
+            alert('Payment sucessful')
+        }).catch(err =>{
+            console.log('Payment error: ', JSON.parse(err))
+            alert('There was an issue with your payment')
+        })
+    }
     return (
         <StripeCheckout
             currency='PLN'
